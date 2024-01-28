8ea499e13ab619a4efa1d4b0c60f85417a6352b8
Beno <circiubeni@gmail.com>
2024-01-28 12:38:02 +0200

    First Version

--- src/main/java/com/example/tictactoe/TicTacToe.java
+++ src/main/java/com/example/tictactoe/TicTacToe.java
@@ -1,6 +1,5 @@
 package com.example.tictactoe;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,7 +12,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class TicTacToe extends Application {
 
     private void FreshStart()
     {
